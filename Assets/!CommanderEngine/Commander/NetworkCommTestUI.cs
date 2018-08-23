505075d5d4dcfb5a4d2b160a2ecc5c98e765d383
DESKTOP-5PJFMVU\User <jirka.polci@gmail.com>
2018-08-23 16:31:49 +0200

    2way 2
    This reverts commit 15aa8ac34f0e4c4975c629341097770a7153091d.

---
+++ Assets/!CommanderEngine/Commander/NetworkCommTestUI.cs
@@ -0,0 +1,47 @@
+﻿using CommanderEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkCommTestUI : MonoBehaviour
+{
+
+    private string who = "Joe";
+    private string what = "sit";
+    private string where = "Chair";
+    private int num = 1;
+    private string command;
+
+    public Text sendButtonText;
+
+    public string Who { get { return who; } set { who = value; UpdateCommand(); } }
+
+    public string What { get { return what; } set { what = value; UpdateCommand(); } }
+
+    public int Num { get { return num; } set { num = value; UpdateCommand(); } }
+
+    public string Where { get { return where; } set { where = value; UpdateCommand(); } }
+
+    //
+    public void UpdateCommand()
+    {
+        command =
+            "ow" + Command.valSep + who + Command.parDel +
+            "cmd" + Command.valSep + what + Command.parDel +
+            "tgt" + Command.valSep + where + num;
+
+            //(what != "standup" ? "tgt" + Command.valSep + where + num : "");
+        sendButtonText.text = "Send: " + command;
+    }
+
+    public void Do()
+    {
+        Commander.Do(command);
+    }
+
+    private void Start()
+    {
+        UpdateCommand();
+    }
+}
